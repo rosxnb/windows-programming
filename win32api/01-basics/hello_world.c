8885f7fdba33d06841cb7a883387cb5aee8a3e23
Roshan Bharati <rosxnb@gmail.com>
2025-02-09 12:12:51 +0545

    hello window

---
+++ win32api/01-basics/hello_world.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PSTR szCmdLine,
+    int iCmdShow
+) {
+    MessageBox( NULL, TEXT("Hello, Windows!!"), TEXT("HelloMsg"), 0 );
+    return 0;
+}
